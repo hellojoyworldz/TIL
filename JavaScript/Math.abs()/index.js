00a5f3c437206f1b745237ef9363880737df791b
hellojoyworldz <hellojoyworldz@gmail.com>
2024-02-12 10:06:12 +0900

    Math.abs()

---
+++ JavaScript/Math.abs()/index.js
@@ -0,0 +1,10 @@
+Math.abs("-1"); // 1
+Math.abs(-2); // 2
+Math.abs(null); // 0
+Math.abs(""); // 0
+Math.abs([]); // 0
+Math.abs([2]); // 2
+Math.abs([1, 2]); // NaN
+Math.abs({}); // NaN
+Math.abs("string"); // NaN
+Math.abs(); // NaN
