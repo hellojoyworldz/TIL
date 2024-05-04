d85568f92c182290683ea76f129268329bdc4d47
hellojoyworldz <hellojoyworldz@gmail.com>
2024-05-05 00:44:56 +0900

    MDN 파일로 이동

--- JavaScript/Array.prototype.shift()/index.js
+++
@@ -1,6 +0,0 @@
-let ary = [1,2,3,4,5]
-console.log(ary.shift())
-console.log(ary)
-
-console.log(ary.shift())
-console.log(ary)
--- JavaScript/Array.prototype.unshift()/index.js
+++
@@ -1,6 +0,0 @@
-let ary = [1,2,3]
-console.log(ary.unshift(0)) // the new array length
-console.log(ary)
-
-console.log(ary.unshift(-2,-1)) // the new array length
-console.log(ary)
